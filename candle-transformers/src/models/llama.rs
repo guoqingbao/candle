73398b51f471bb4009820e2bc89d925d216efebd
Guoqing Bao <guoqing.bao@enflame-tech.com>
2024-03-05 10:30:04 +0000

    Revert llama model for higher precision attention

--- candle-transformers/src/models/llama.rs
+++ candle-transformers/src/models/llama.rs
@@ -267,17 +267,17 @@ impl CausalSelfAttention {
             let softmax_scale = 1f32 / (self.head_dim as f32).sqrt();
             flash_attn(&q, &k, &v, softmax_scale, seq_len > 1)?.transpose(1, 2)?
         } else {
-            // let in_dtype = q.dtype();
-            // let q = q.to_dtype(DType::F32)?;
-            // let k = k.to_dtype(DType::F32)?;
-            // let v = v.to_dtype(DType::F32)?;
+            let in_dtype = q.dtype();
+            let q = q.to_dtype(DType::F32)?;
+            let k = k.to_dtype(DType::F32)?;
+            let v = v.to_dtype(DType::F32)?;
             let att = (q.matmul(&k.t()?)? / (self.head_dim as f64).sqrt())?;
             let mask = self.cache.mask(seq_len)?.broadcast_as(att.shape())?;
             let att = masked_fill(&att, &mask, f32::NEG_INFINITY)?;
             // let att = candle_nn::ops::softmax(&att, D::Minus1)?;
             let att = candle_nn::ops::softmax_last_dim(&att)?;
             // Convert to contiguous as matmul doesn't support strided vs for now.
-            att.matmul(&v.contiguous()?)?
+            att.matmul(&v.contiguous()?)?.to_dtype(in_dtype)?
         };
         let y = y.transpose(1, 2)?.reshape(&[b_sz, seq_len, hidden_size])?;
         let y = self.o_proj.forward(&y)?;
